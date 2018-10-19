c923045bb82813a8f0275e499185b38a52bff81e
seirion <seirion@mensakorea.org>
2018-10-19 15:04:55 +0900

    remove test code

--- app/src/main/java/com/seirion/videoapp/MainActivity.kt
+++ app/src/main/java/com/seirion/videoapp/MainActivity.kt
@@ -1,23 +1,12 @@
 package com.seirion.videoapp
 
 import android.os.Bundle
-import android.util.Log
-import com.seirion.videoapp.base.ActivityLifecycle
 import com.seirion.videoapp.base.BaseAppCompatActivity
-import io.reactivex.Observable
-import java.util.concurrent.TimeUnit
 
 class MainActivity : BaseAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    // 테스트 코드임 (삭제 예정)
-    fun test() {
-        Observable.interval(1, TimeUnit.SECONDS)
-                .takeUntil(getLifecycleSignal(ActivityLifecycle.DESTROY))
-                .subscribe { Log.e("", it.toString()) }
     }
 }
